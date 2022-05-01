6e7862c2c5359dc86ad9b086a3a2ab07beb3723d
lucasmaciiel <lucasmacielgois@gmail.com>
2022-05-01 14:49:55 -0300

    Mensagens de erro

--- src/main/java/com/lmg/pedidos/model/dto/RequisicaoNovoPedido.java
+++ src/main/java/com/lmg/pedidos/model/dto/RequisicaoNovoPedido.java
@@ -1,18 +1,29 @@
 package com.lmg.pedidos.model.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.lmg.pedidos.model.Pedido;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+
 @ToString
 @Getter
 @Setter
-public class PedidoDTO {
+public class PedidoDto {
 
+    @NotBlank
     private String nomeProduto;
+
+    @NotBlank
     private String urlProduto;
+
+    @NotBlank
     private String urlImagem;
+
+    @NotBlank
     private String descricao;
 
     public Pedido pedidoMapper() {
