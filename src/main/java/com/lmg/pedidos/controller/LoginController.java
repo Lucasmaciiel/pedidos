46b1821aca999c4b6e13c052e7f27f925f487c6b
lucasmaciiel <lucasmacielgois@gmail.com>
2022-05-02 15:20:33 -0300

    Página de login

--- src/main/java/com/lmg/pedidos/controller/LoginController.java
+++ src/main/java/com/lmg/pedidos/controller/LoginController.java
@@ -1,2 +1,15 @@
-package com.lmg.pedidos.controller;public class LoginController {
+package com.lmg.pedidos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public String login(){
+        return "login";
+    }
 }
